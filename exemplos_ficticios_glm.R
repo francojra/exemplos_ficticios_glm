f71fb5a0df9840ce25511a259f3eff23b22d5009
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-07-10 18:58:09 -0300

    Add files via upload

---
+++ exemplos_ficticios_glm.R
@@ -0,0 +1,43 @@
+# Criar o conjunto de dados fictício
+dados <- data.frame(
+  tempo_estudo = c(2, 4, 3, 6, 8),
+  consumo_cafe = c(1, 2, 3, 2, 1),
+  exercicio = c(30, 20, 40, 50, 60),
+  concentracao = c(5, 6, 7, 8, 9),
+  sono = c(5, 6, 10, 12, 8),
+  desempenho = c(60, 65, 70, 75, 80)
+)
+
+# Aplicar a função GLM com todas as combinações
+modelo <- glm(desempenho ~ ., data = dados, family = Gamma)
+modelo1 <- glm(desempenho ~ . * ., data = dados, family = Gamma)
+
+# Visualizar os resultados
+summary(modelo)
+summary(modelo1)
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Criar o conjunto de dados fictício
+dados <- data.frame(
+  tempo_estudo = c(2, 4, 3, 5, 6),
+  consumo_cafe = c(1, 2, 3, 2, 1),
+  exercicio = c(30, 20, 40, 50, 60),
+  concentracao = c(5, 6, 7, 8, 9),
+  sono = c(7, 8, 6, 7, 8),
+  desempenho = c(60, 65, 70, 75, 80)
+)
+
+# Criar todas as combinações possíveis entre as variáveis explicativas
+comb_variaveis <- combn(names(dados)[1:5], 1)
+interacoes <- apply(comb_variaveis, 2, paste, collapse = "*")
+termos <- c(names(dados)[1:5], interacoes)
+
+# Aplicar a função GLM com todas as combinações
+formula <- paste("desempenho ~", paste(termos, collapse = "+"))
+modelo <- glm(formula, data = dados)
+
+# Visualizar os resultados
+summary(modelo)
